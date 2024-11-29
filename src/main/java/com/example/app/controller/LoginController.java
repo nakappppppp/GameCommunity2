d1f52cf3efcd95acf1b670c6084740121248c960
github nakappppppp <pbaransu@gmail.com>
2024-11-29 13:31:14 +0900

    開発１（image）

--- src/main/java/com/example/app/controller/LoginController.java
+++ src/main/java/com/example/app/controller/LoginController.java
@@ -2,10 +2,11 @@ package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-//@RequestMapping("/GameHive")
+@RequestMapping("/GameHive")
 public class LoginController {
 
 	@GetMapping("/login")
